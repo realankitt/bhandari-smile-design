ee8fa9cca9c4c9420dece6536e8033d643126fe8
Retr0 <dedsecretr0680@gmail.com>
2025-04-30 16:33:04 +0530

    fix: correct HomePage component reference to Home in App routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Index';
 import BlogPage from './pages/Blog';
 import BlogArticle from './pages/BlogArticle';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<Home />} />
         <Route path="/blog" element={<BlogPage />} />
         <Route path="/blog/:slug" element={<BlogArticle />} />
         <Route path="*" element={<NotFound />} />
@@ -14,3 +16,5 @@ function App() {
     </BrowserRouter>
   );
 }
+
+export default App;
