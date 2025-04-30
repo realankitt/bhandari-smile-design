2c61c814815fa147ad77c1974ddead8ff6dc74e1
Retr0 <dedsecretr0680@gmail.com>
2025-04-30 16:36:07 +0530

    feat: wrap application in QueryClientProvider for React Query integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,25 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Home from './pages/Index';
 import BlogPage from './pages/Blog';
 import BlogArticle from './pages/BlogArticle';
 import NotFound from './pages/NotFound';
 
+// Create a client
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/blog" element={<BlogPage />} />
-        <Route path="/blog/:slug" element={<BlogArticle />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/blog" element={<BlogPage />} />
+          <Route path="/blog/:slug" element={<BlogArticle />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
