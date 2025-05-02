447aaece3d10e8bcaac06d3821207de2f05985cc
Retr0 <dedsecretr0680@gmail.com>
2025-05-02 13:26:14 +0530

    refactor: remove TinaCMS integration and related configurations

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { TinaProvider, TinaCMS } from 'tinacms';
 import Home from './pages/Index';
 import BlogPage from './pages/Blog';
 import BlogArticle from './pages/BlogArticle';
@@ -15,30 +14,18 @@ const queryClient = new QueryClient({
   }
 });
 
-const cms = new TinaCMS({
-  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID || '',
-  branch: 'main', // Changed from 'remake' to 'main'
-  token: process.env.TINA_TOKEN,
-  mediaStore: async () => {
-    const pack = await import('next-tinacms-cloudinary');
-    return pack.TinaCloudCloudinaryMediaStore;
-  },
-});
-
 function App() {
   return (
-    <TinaProvider cms={cms}>
-      <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/blog" element={<BlogPage />} />
-            <Route path="/blog/:slug" element={<BlogArticle />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
-      </QueryClientProvider>
-    </TinaProvider>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/blog" element={<BlogPage />} />
+          <Route path="/blog/:slug" element={<BlogArticle />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
